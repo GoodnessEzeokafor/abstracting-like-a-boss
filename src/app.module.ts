5fbf00b596b704955be2debe829ff7f453ae51e5
Goodness Ezeokafor <gootech442@gmail.com>
2024-06-05 22:45:15 +0100

    chore: removed app.controller.spec.ts, app.controller.ts and app.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseServicesModule } from './services/database-services/database-services.module';
 
 @Module({
   imports: [DatabaseServicesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
